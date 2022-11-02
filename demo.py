7f6b4d8bd8cba4190f9c35ce8b416c774700b593
Gariscat <xl3133@nyu.edu>
2022-11-03 01:10:09 +0800

    stuck at training...

--- demo.py
+++ demo.py
@@ -7,7 +7,14 @@ env = PulseEnv('./traces/trace.log', source_id=3, obs_ord=3, num_gaps=256)
 
 policy_kwargs = dict(
     features_extractor_class=RNNEncoder,
-    features_extractor_kwargs=dict(features_dim=32),
+    features_extractor_kwargs=dict(features_dim=32, num_layers=1),
 )
-model = DDPG('MlpPolicy', env, buffer_size=int(1e5), policy_kwargs=policy_kwargs, verbose=1)
-model.learn(int(1e6))
+model = DDPG('MlpPolicy', env, buffer_size=int(1e5), policy_kwargs=policy_kwargs, verbose=2)
+# model.learn(100, log_interval=10)
+obs = env.reset()
+while True:
+    action, _states = model.predict(obs)
+    obs, rewards, dones, info = env.step(action)
+    print(action, rewards)
+    if dones:
+        break
