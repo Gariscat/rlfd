a8e0b6253526a8aacf8fa6b2c89bff6ea998fb3d
Gariscat <xl3133@nyu.edu>
2022-11-12 21:32:05 +0800

    grid_exp change parameters

---
+++ utils.py
@@ -0,0 +1,28 @@
+from tqdm import trange
+import numpy as np
+
+
+def evaluate(env, model, num_episodes=100):
+    criteria = {
+        'step_rewards': [],
+        'episode_rewards': [],
+        'avg_step_reward': 0,
+        'avg_episode_reward': 0,
+    }
+    for i_episode in trange(num_episodes, desc='Evaluation'):
+        obs = env.reset()
+        done = False
+        r = 0
+        while not done:
+            action, _ = model.predict(obs)
+            obs, reward, done, info = env.step(action)
+
+            criteria['step_rewards'] += [reward]
+            r += reward
+
+        criteria['episode_rewards'] += [r]
+
+    criteria['avg_step_reward'] = np.mean(criteria['step_rewards'])
+    criteria['avg_episode_reward'] = np.mean(criteria['episode_rewards'])
+
+    return criteria
